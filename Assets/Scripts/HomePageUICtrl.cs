e3e87ef84bf721ec7def49f6a147cd5dd5f27f0e
Sanboryan <lokshang@gmail.com>
2024-05-15 03:19:43 +0800

    2024年5月15日03:19:42

--- Assets/Scripts/HomePageUICtrl.cs
+++ Assets/Scripts/HomePageUICtrl.cs
@@ -7,7 +7,7 @@ public class HomePageUICtrl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("test test test test");
     }
 
     // Update is called once per frame
